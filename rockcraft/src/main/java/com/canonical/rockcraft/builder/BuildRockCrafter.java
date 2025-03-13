ef2867b996326e6db2505b1b523f728ce53b1c14
Vladimir Petko <vladimir.petko@canonical.com>
2025-03-13 14:32:08 +1300

    fix: require BuildRockcraftOptions in BuildRockCrafter constructor

--- rockcraft/src/main/java/com/canonical/rockcraft/builder/BuildRockCrafter.java
+++ rockcraft/src/main/java/com/canonical/rockcraft/builder/BuildRockCrafter.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 public class BuildRockCrafter extends AbstractRockCrafter {
-    public BuildRockCrafter(RockProjectSettings settings, CommonRockcraftOptions options, List<File> artifacts) {
+    public BuildRockCrafter(RockProjectSettings settings, BuildRockcraftOptions options, List<File> artifacts) {
         super(settings, options, artifacts);
     }
 
