9e8a7f330bfa05d84647b7690d36b1002c605b4b
Vladimir Petko <vladimir.petko@canonical.com>
2025-03-13 14:33:11 +1300

    fix: add factory method to create builder RockSettings

--- rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/RockSettingsFactory.java
+++ rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/RockSettingsFactory.java
@@ -1,10 +1,10 @@
 package com.canonical.rockcraft.gradle;
 
+import com.canonical.rockcraft.builder.IRockcraftNames;
 import org.gradle.api.Project;
 
 import com.canonical.rockcraft.builder.Generator;
 import com.canonical.rockcraft.builder.RockProjectSettings;
-import org.gradle.api.invocation.Gradle;
 
 /**
  * Creates RockProjectSettings from Gradle project
@@ -30,4 +30,19 @@ public class RockSettingsFactory {
                 !project.getTasksByName(ITaskNames.JLINK, false).isEmpty() ||
                         !project.getTasksByName(ITaskNames.RUNTIME, false).isEmpty());
     }
+
+    /**
+     * Creates RockProjectSettings from Gradle project for the build rock
+     *
+     * @param project - gradle project
+     * @return RockProjectSettings
+     */
+    public static final RockProjectSettings createBuildRockProjectSettings(Project project) {
+        return new RockProjectSettings(Generator.gradle, project.getGradle().getGradleVersion(),
+                project.getName(), String.valueOf(project.getVersion()),
+                project.getProjectDir().toPath(), project.getLayout().getBuildDirectory().getAsFile().get().toPath().resolve(IRockcraftNames.BUILD_ROCK_OUTPUT),
+                !project.getTasksByName(ITaskNames.JLINK, false).isEmpty() ||
+                        !project.getTasksByName(ITaskNames.RUNTIME, false).isEmpty());
+    }
+
 }
