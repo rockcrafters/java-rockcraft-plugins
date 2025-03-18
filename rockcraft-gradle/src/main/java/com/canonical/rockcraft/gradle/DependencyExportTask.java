f2cf2911d9437af888ada989b4e9b9d9ce2e54c8
Vladimir Petko <vladimir.petko@canonical.com>
2025-03-18 15:59:59 +1300

    fix(DependencyExport): multiple fixes
    - copy BOM contents to the outptu directory
    - set Maven scopes for export
    - copy POMs of the resolved artifacts

--- rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/DependencyExportTask.java
+++ rockcraft-gradle/src/main/java/com/canonical/rockcraft/gradle/DependencyExportTask.java
@@ -37,17 +37,11 @@ import org.gradle.maven.MavenModule;
 import org.gradle.maven.MavenPomArtifact;
 
 import javax.inject.Inject;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 /**
  * DependencyExportTask writes the project build dependencies to the output
@@ -76,49 +70,59 @@ public abstract class DependencyExportTask extends DefaultTask {
 
     /**
      * Task action to write dependencies
-     * @throws IOException - failed to write project dependencies
      */
     @TaskAction
     public void export() throws IOException {
-        for (String configName : dependencyOptions.getConfigurations()) {
-            Configuration config = this.getProject().getConfigurations().findByName(configName);
-            if (config == null) {
-                throw new IllegalArgumentException(String.format("Configuration %s not found.", configName));
-            }
+        Path outputLocationRoot = getOutputDirectory().getAsFile().get().toPath();
+        ArtifactCopy artifactCopy = new ArtifactCopy(outputLocationRoot);
+
+
+        this.getProject().getConfigurations().all( config -> {
             if (!config.isCanBeResolved()) {
                 logger.warn(String.format("Configuration %s can not be resolved. skipped.", config.getName()));
-                continue;
+                return;
             }
             PomDependencyReader pomDependencyReader = new PomDependencyReader(getProject().getDependencies(),
-                    getProject().getConfigurations());
-            copyConfiguration(pomDependencyReader, config, getProject().getDependencies());
-        }
+                    getProject().getConfigurations(), artifactCopy);
+            copyConfiguration(pomDependencyReader, config, getProject().getDependencies(), artifactCopy);
+        });
+
         // export build script dependencies
-        final PomDependencyReader pomDependencyReader = new PomDependencyReader(getProject().getDependencies(),
-                getProject().getConfigurations());
+        final PomDependencyReader pomDependencyReader = new PomDependencyReader(getProject().getBuildscript().getDependencies(),
+                getProject().getBuildscript().getConfigurations(), artifactCopy);
 
         this.getProject()
                 .getBuildscript()
                 .getConfigurations()
                 .forEach( x ->
-                        copyConfiguration(pomDependencyReader, x, getProject().getBuildscript().getDependencies()));
+                        copyConfiguration(pomDependencyReader, x, getProject().getBuildscript().getDependencies(), artifactCopy));
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
-    private void copyConfiguration(PomDependencyReader pomDependencyReader, Configuration files, DependencyHandler handler) {
+    private void copyConfiguration(PomDependencyReader pomDependencyReader, Configuration files, DependencyHandler handler, ArtifactCopy artifactCopy) {
         try {
-            copyArtifacts(files.getIncoming().getArtifacts());
+
+            artifactCopy.copyArtifacts(files.getIncoming().getArtifacts());
             // resolve and copy POM files
-            Path outputLocationRoot = getOutputDirectory().getAsFile().get().toPath();
             HashSet<ComponentIdentifier> workQueue = new HashSet<ComponentIdentifier>();
+            for (ResolvedArtifactResult result : files.getIncoming().getArtifacts()) {
+                ComponentIdentifier id = result.getVariant().getOwner();
+                workQueue.add(id);
+                logger.debug("Looking up POM from Incoming "+ id);
+            }
             for (Dependency result : files.getAllDependencies()) {
                 if (result.getVersion() != null) {
                     ModuleComponentIdentifier id = DefaultModuleComponentIdentifier.newId(DefaultModuleIdentifier.newId(result.getGroup(), result.getName()), result.getVersion());
-                    workQueue.add(DefaultModuleComponentIdentifier.newId(DefaultModuleIdentifier.newId(result.getGroup(), result.getName()), result.getVersion()));
-                    logger.debug("Looking up POM for "+ id);
+                    workQueue.add(id);
+                    logger.debug("Looking up POM from AllDeps "+ id);
                 }
             }
-            HashSet<ComponentIdentifier> resolved = new HashSet<ComponentIdentifier>();
+            Set<String> scopes = new HashSet<>();
+            scopes.add("compile");
+            scopes.add("import");
+            scopes.add("runtime");
+            HashSet<ComponentIdentifier> dependencyManagementResolved = new HashSet<>();
+            HashSet<ComponentIdentifier> resolved = new HashSet<>();
             while (!workQueue.isEmpty()) {
                 ArtifactResolutionResult artifacts = handler
                         .createArtifactResolutionQuery()
@@ -131,14 +135,12 @@ public abstract class DependencyExportTask extends DefaultTask {
                     if (component.getId() instanceof ModuleComponentIdentifier) {
                         for (ArtifactResult artifact : component.getArtifacts(MavenPomArtifact.class)) {
                             logger.debug("Found artifact " + artifact.getId());
-                            copyToMavenRepository(((ResolvedArtifactResult) artifact), outputLocationRoot);
+                            artifactCopy.copyToMavenRepository(((ResolvedArtifactResult) artifact));
                             // resolve maven dependencies to fetch poms
-                            Set<ComponentIdentifier> componentIds = pomDependencyReader.read(((ResolvedArtifactResult) artifact).getFile());
-                            for (ComponentIdentifier ci : componentIds){
-                                if (!resolved.contains(ci)) {
-                                    workQueue.add(ci);
-                                }
-                            }
+                            DependencyResolutionResult dependencies = pomDependencyReader.read(((ResolvedArtifactResult) artifact).getFile(), scopes);
+                            workQueue.addAll(dependencies.dependencies().stream().filter(x -> !resolved.contains(x)).toList());
+                            copyBoms(handler,artifactCopy, new HashSet<>(dependencies.dependencyManagement().stream().filter( x -> !dependencyManagementResolved.contains(x)).toList()));
+                            dependencyManagementResolved.addAll(dependencies.dependencyManagement());
                         }
                     }
                 }
@@ -149,60 +151,20 @@ public abstract class DependencyExportTask extends DefaultTask {
         }
     }
 
-    private void copyArtifacts(ArtifactCollection artifacts) throws IOException {
-        Path outputLocationRoot = getOutputDirectory().getAsFile().get().toPath();
-        for (ResolvedArtifactResult result : artifacts.getArtifacts()) {
-            copyToMavenRepository(result, outputLocationRoot);
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private void copyBoms(DependencyHandler handler, ArtifactCopy artifactCopy, Set<ComponentIdentifier> componentIdentifiers) throws IOException {
+        ArtifactResolutionResult artifacts = handler
+                .createArtifactResolutionQuery()
+                .forComponents(componentIdentifiers)
+                .withArtifacts(MavenModule.class, new Class[]{MavenPomArtifact.class})
+                .execute();
+        for (ComponentArtifactsResult component : artifacts.getResolvedComponents()) {
+            if (component.getId() instanceof ModuleComponentIdentifier) {
+                for (ArtifactResult artifact : component.getArtifacts(MavenPomArtifact.class)) {
+                    logger.debug("Found artifact " + artifact.getId());
+                    artifactCopy.copyToMavenRepository(((ResolvedArtifactResult) artifact));
+                }
+            }
         }
-    }
-
-    private void copyToMavenRepository(ResolvedArtifactResult resolvedArtifact, Path outputLocationRoot ) throws IOException {
-        File f = resolvedArtifact.getFile();
-        StringTokenizer tk = new StringTokenizer(resolvedArtifact.getId().getComponentIdentifier().getDisplayName(), ":");
-        String group = null;
-        if (tk.hasMoreTokens()) {
-            group = tk.nextToken();
-        }
-        String name = null;
-        if (tk.hasMoreTokens()) {
-            name = tk.nextToken();
-        }
-        String version = null;
-        if (tk.hasMoreTokens()) {
-            version = tk.nextToken();
-        }
-        copyToMavenRepository(f, group, name, version, outputLocationRoot);
-    }
-
-    private void copyToMavenRepository(File f, String group, String name, String version, Path outputLocationRoot ) throws IOException {
-        if (group == null || name == null || version == null) {
-            throw new IllegalArgumentException(String.format("Group, name and version should be set for the artifact %s:%s:%s", group, name, version));
-        }
-        Path outputLocation = outputLocationRoot.resolve(String.format("%s/%s/%s", group.replace('.', '/'), name, version));
-        outputLocation.toFile().mkdirs();
-        Path destinationFile = outputLocation.resolve(f.getName());
-        Files.copy(f.toPath(), destinationFile, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
-
-        try {
-            Path digestFile = Path.of(destinationFile.toString() + ".sha1");
-            String hash = computeHash(destinationFile, "sha1");
-            String paddedSha1 = String.format("%40s", hash).replace(' ', '0');
-            Files.writeString(digestFile, paddedSha1);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        logger.debug(String.format("Written %s and corresponding sha1", destinationFile));
-    }
-
-    private static String computeHash(Path filePath, String alg) throws IOException, NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance(alg);
-        byte[] bytes = Files.readAllBytes(filePath);
-        digest.update(bytes, 0, bytes.length);
-        byte[] hash = digest.digest();
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : hash) {
-            hexString.append(String.format("%02x", b));
-        }
-        return hexString.toString();
     }
 }
