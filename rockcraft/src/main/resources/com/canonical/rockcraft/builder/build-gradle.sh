6e73a9ea3d4ab421c0c9356b2b5087f6a542c38d
Vladimir Petko <vladimir.petko@canonical.com>
2025-04-09 19:33:59 +1200

    feat: write init file and starter scripts

---
+++ rockcraft/src/main/resources/com/canonical/rockcraft/builder/build-gradle.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+WORKDIR=${1:-`pwd`}
+if [ $# -gt 0 ]; then
+    shift
+fi
+if [ $# -eq 0 ]; then
+    TASK=build -x checkRockcraft
+else
+    TASK=$@
+fi
+(cd ${WORKDIR} && gradle $TASK)
