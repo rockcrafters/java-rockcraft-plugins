6bfe31c5e4d1663be237f70fd4ba78e55d56cc58
Vladimir Petko <vladimir.petko@canonical.com>
2025-07-04 19:28:44 +1200

    fix: use spawned process to populate dependencies for a given goal

--- rockcraft-maven/src/main/java/com/canonical/rockcraft/maven/CreateBuildRockMojo.java
+++ rockcraft-maven/src/main/java/com/canonical/rockcraft/maven/CreateBuildRockMojo.java
@@ -18,11 +18,13 @@ import org.apache.maven.model.building.DefaultModelBuildingRequest;
 import org.apache.maven.model.building.ModelBuildingException;
 import org.apache.maven.model.building.ModelBuildingRequest;
 import org.apache.maven.model.building.ModelBuildingResult;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.plugins.dependency.resolvers.GoOfflineMojo;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
@@ -33,11 +35,29 @@ import org.apache.maven.shared.transfer.artifact.resolve.ArtifactResult;
 import org.apache.maven.shared.transfer.dependencies.DefaultDependableCoordinate;
 import org.apache.maven.shared.transfer.dependencies.resolve.DependencyResolverException;
 import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.impl.RemoteRepositoryManager;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -49,26 +69,15 @@ import java.util.Set;
 /**
  * Writes build rock rockcraft file to the output directory
  */
-@Mojo(name = "create-build-rock", threadSafe = false, requiresProject = true, defaultPhase = LifecyclePhase.PACKAGE)
-public final class CreateBuildRockMojo extends GoOfflineMojo {
-
-    private final Collection<String> collectionScopes = Arrays.asList("system", "compile", "test", "provided", "runtime");
-    private final Collection<String> resolveScopes = Arrays.asList("system", "compile", "test", "provided", "runtime");
-
-    @Component
-    private LifecycleDependencyResolver lifecycleDependencyResolver;
-
-    @Component
-    private ProjectBuilder projectBuilder;
+@Mojo(name = "create-build-rock", threadSafe = false, requiresProject = true,
+        requiresDependencyResolution = ResolutionScope.COMPILE)
+public final class CreateBuildRockMojo extends AbstractMojo {
 
     @Component
     private RuntimeInformation runtimeInformation;
 
     @Component
-    private RepositorySystem repositorySystem;
-
-    @Component
-    private RemoteRepositoryManager remoteRepositoryManager;
+    private MavenProject project;
 
     @Parameter(property = "buildPackage")
     private final String buildPackage = "openjdk-21-jdk-headless";
@@ -108,6 +117,18 @@ public final class CreateBuildRockMojo extends GoOfflineMojo {
 
     @Parameter(defaultValue = "${maven.home}", readonly = true, required = true)
     private File mavenHome;
+
+    @Parameter(property = "buildCommand", defaultValue = "package")
+    private String buildArgs;
+
+    @Parameter(property = "run-build.workingDirectory", defaultValue = "${project.basedir}")
+    private File workingDirectory;
+
+    @Parameter(defaultValue = "${repositorySystemSession}", readonly = true)
+    private RepositorySystemSession repoSession;
+
+    @Parameter(property = "allowLocalCache", defaultValue = "true")
+    private boolean allowLocalCache;
 
     private final BuildRockcraftOptions options = new BuildRockcraftOptions();
 
@@ -151,137 +172,143 @@ public final class CreateBuildRockMojo extends GoOfflineMojo {
      *
      * @throws MojoExecutionException - generation failure
      */
-    protected void doExecute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException {
         configure();
 
-        RockProjectSettings settings = RockSettingsFactory.createBuildRockProjectSettings(getRuntimeInformation(), getProject());
-
+        RockProjectSettings settings = RockSettingsFactory.createBuildRockProjectSettings(getRuntimeInformation(), project);
         Path dependenciesOutput = settings.getRockOutput().resolve(IRockcraftNames.DEPENDENCIES_ROCK_OUTPUT);
-        dependenciesOutput.toFile().mkdirs();
+        Path mavenExecutable = mavenHome.toPath().resolve("bin/mvn");
+        if (!Files.exists(mavenExecutable)) {
+            throw new MojoExecutionException(mavenExecutable + ": maven executable file does not exist!");
+        }
         try {
-            // resolve the project
-            MavenArtifactCopy artifactCopy = new MavenArtifactCopy(dependenciesOutput);
-            String baseDir = session.getLocalRepository().getBasedir();
+            Path buildPom = project.getModel().getPomFile().toPath();
+            if (allowLocalCache) {
+                Path temp = Files.createTempFile(workingDirectory.toPath(), "tempPom", ".xml");
+                temp.toFile().deleteOnExit();
+                Files.copy(project.getModel().getPomFile().toPath(), temp , StandardCopyOption.REPLACE_EXISTING);
 
-            Set<Artifact> myArtifacts = new HashSet<>();
-            MavenProject copy = new MavenProject(getProject());
-            copy.setArtifactFilter(new ArtifactFilter() {
-                @Override
-                public boolean include(Artifact artifact) {
-                    return true;
-                }
-            });
-            // resolve lifecycle artifacts
-            lifecycleDependencyResolver.resolveProjectDependencies(copy,
-                    collectionScopes,
-                    resolveScopes,
-                    session,
-                    false,
-                    myArtifacts);
-            myArtifacts.addAll(copy.getArtifacts());
-            myArtifacts.addAll(resolveDependencyArtifacts());
-            myArtifacts.addAll(resolvePlugins());
+                File localRepo = repoSession.getLocalRepository().getBasedir();
 
-            for (Artifact dep : myArtifacts) {
-                copyArtifacts(baseDir, dep.getGroupId(), dep.getArtifactId(), dep.getVersion(), artifactCopy);
-                File pomFile = POMLookup.lookup(dep,
-                        session.getCurrentProject().getRemotePluginRepositories(),
-                        session.getRepositorySession(),
-                        repositorySystem);
-                // copies parent poms
-                copyPOMFiles(artifactCopy, pomFile);
+                addRepositoryToPom(temp.toFile(),
+                        "local-maven-cache"+ System.currentTimeMillis(),
+                        "local maven repository",
+                        "file://"+localRepo);
+                buildPom = temp;
             }
 
-            // copies poms for parent/dependencies
-            copyPOMFiles(artifactCopy, getProject().getFile());
+            ProcessBuilder pb = new ProcessBuilder("mvn",
+                    "-Dmaven.repo.local="+dependenciesOutput,
+                    "-f", buildPom.toString(),
+                    buildArgs)
+                    .redirectErrorStream(true)
+                    .directory(workingDirectory);
+            dependenciesOutput.toFile().mkdirs();
+
+            Process process = pb.start();
+
+            try (InputStream is = process.getInputStream()) {
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = is.read(buffer)) != -1) {
+                    getLog().info(new String(buffer, 0, len).trim());
+                }
+            }
+            int exitCode = process.waitFor();
+            if (exitCode != 0){
+                throw new MojoExecutionException("Failed to build project "+ project.getName() + ", dependencies are not available");
+            }
 
             BuildRockCrafter rockCrafter = new BuildRockCrafter(settings, getOptions(), Collections.singletonList(dependenciesOutput.toFile()));
             rockCrafter.writeRockcraft();
         }
-        catch (IOException | DependencyResolverException | ModelBuildingException | LifecycleExecutionException e) {
+        catch (IOException | InterruptedException | ParserConfigurationException | SAXException | TransformerException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 
-    private Set<Artifact> resolvePlugins() throws DependencyResolverException {
-        HashSet<Artifact> arts = new HashSet<>();
+    public static void addRepositoryToPom(File pomFile,
+                                          String repoId,
+                                          String repoName,
+                                          String repoUrl)
+            throws ParserConfigurationException, IOException, SAXException, TransformerException {
 
-        for (org.apache.maven.model.Plugin p : getProject().getBuildPlugins()) {
-            if (!p.getDependencies().isEmpty()) {
-                System.out.println("Foo");
-            }
-            DefaultDependableCoordinate dc = new DefaultDependableCoordinate();
-            dc.setGroupId(p.getGroupId());
-            dc.setArtifactId(p.getArtifactId());
-            dc.setVersion(p.getVersion());
-            Iterable<ArtifactResult> result = getDependencyResolver()
-                    .resolveDependencies(newResolveArtifactProjectBuildingRequest(), dc, null);
-            for (ArtifactResult r :  result) {
-                arts.add(r.getArtifact());
-            }
-        }
-        for (org.apache.maven.model.ReportPlugin p : getProject().getReportPlugins()) {
-            DefaultDependableCoordinate dc = new DefaultDependableCoordinate();
-            dc.setGroupId(p.getGroupId());
-            dc.setArtifactId(p.getArtifactId());
-            dc.setVersion(p.getVersion());
-            Iterable<ArtifactResult> result = getDependencyResolver()
-                    .resolveDependencies(newResolveArtifactProjectBuildingRequest(), dc, null);
-            for (ArtifactResult r :  result) {
-                arts.add(r.getArtifact());
-            }
-        }
-        DefaultDependableCoordinate dc = new DefaultDependableCoordinate();
-        dc.setGroupId("org.codehaus.plexus");
-        dc.setArtifactId("plexus-utils");
-        dc.setVersion("1.1");
-        Iterable<ArtifactResult> result = getDependencyResolver()
-                .resolveDependencies(newResolveArtifactProjectBuildingRequest(), dc, null);
-        for (ArtifactResult r :  result) {
-            arts.add(r.getArtifact());
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(pomFile);
+
+        // Get the project element
+        Node projectNode = doc.getElementsByTagName("project").item(0);
+        if (projectNode == null) {
+            throw new IllegalArgumentException("Invalid POM file: 'project' element not found.");
         }
 
-        return arts;
+        // Find or create the <repositories> element
+        Node repositoriesNode = null;
+        Node pluginRepositoriesNode = null;
+        NodeList projectChildren = projectNode.getChildNodes();
+        for (int i = 0; i < projectChildren.getLength(); i++) {
+            Node child = projectChildren.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE && child.getNodeName().equals("repositories")) {
+                repositoriesNode = child;
+            }
+            if (child.getNodeType() == Node.ELEMENT_NODE && child.getNodeName().equals("pluginRepositories")) {
+                pluginRepositoriesNode = child;
+            }
+
+        }
+
+        if (pluginRepositoriesNode == null) {
+            // <repositories> does not exist, create it and append to <project>
+            pluginRepositoriesNode = doc.createElement("pluginRepositories");
+            projectNode.appendChild(pluginRepositoriesNode);
+        }
+        if (repositoriesNode == null) {
+            // <repositories> does not exist, create it and append to <project>
+            repositoriesNode = doc.createElement("repositories");
+            projectNode.appendChild(repositoriesNode);
+        }
+
+        // Create the new <repository> element
+        Element repositoryElement = doc.createElement("repository");
+
+        appendRepositoryDefinition(repoId, repoName, repoUrl, doc, repositoryElement);
+
+        // Append the new <repository> to <repositories>
+        repositoriesNode.appendChild(repositoryElement);
+
+        Element pluginRepositoryElement = doc.createElement("pluginRepository");
+        appendRepositoryDefinition(repoId, repoName, repoUrl, doc, pluginRepositoryElement);
+        pluginRepositoriesNode.appendChild(pluginRepositoryElement);
+
+
+        // Write the updated XML back to the file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // For pretty printing the XML
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); // Adjust indent spaces
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(pomFile);
+        transformer.transform(source, result);
+
+        System.out.println("Repository added/updated successfully in " + pomFile.getAbsolutePath());
     }
 
-    private void copyPOMFiles(MavenArtifactCopy artifactCopy, File pomFile) throws ModelBuildingException {
-        ProjectBuildingRequest buildingRequest = newResolveArtifactProjectBuildingRequest();
-        DefaultModelBuilderFactory factory = new DefaultModelBuilderFactory();
-        DefaultModelBuilder builder = factory.newInstance();
-        DelegatingModelResolver resolver = new DelegatingModelResolver(new ProjectModelResolver(
-                session.getRepositorySession(),
-                null, // RequestTrace, can be null
-                repositorySystem,
-                remoteRepositoryManager,
-                session.getCurrentProject().getRemotePluginRepositories(),
-                ProjectBuildingRequest.RepositoryMerging.POM_DOMINANT,
-                null // ReactorModelPool, can be null
-        ), artifactCopy);
+    private static void appendRepositoryDefinition(String repoId, String repoName, String repoUrl, Document doc, Element repositoryElement) {
+        Element idElement = doc.createElement("id");
+        idElement.setTextContent(repoId);
+        repositoryElement.appendChild(idElement);
 
-        ModelBuildingRequest req = new DefaultModelBuildingRequest();
-        req.setModelResolver(resolver);
-        req.setPomFile(pomFile);
-        req.setSystemProperties(System.getProperties());
-        req.setValidationLevel(ModelBuildingRequest.VALIDATION_LEVEL_MINIMAL);
-        req.setActiveProfileIds(buildingRequest.getActiveProfileIds());
-        req.setInactiveProfileIds(buildingRequest.getInactiveProfileIds());
-        req.setProfiles(buildingRequest.getProfiles());
-        ModelBuildingResult modelResult = builder.build(req);
-        modelResult.getEffectiveModel();
-    }
+        Element nameElement = doc.createElement("name");
+        nameElement.setTextContent(repoName);
+        repositoryElement.appendChild(nameElement);
 
-    private void copyArtifacts(String baseDir, String groupId, String artifactId, String versionId, MavenArtifactCopy artifactCopy) throws IOException {
-        for (File f : getArtifactFiles(baseDir, groupId, artifactId, versionId)) {
-            artifactCopy.copyToMavenRepository(f, groupId, artifactId, versionId);
-        }
-    }
-
-    private File[] getArtifactFiles(String baseDir, String groupId, String artifactId, String versionId) {
-        String artifactPath = baseDir + File.separator
-                + groupId.replace('.', File.separatorChar) + File.separator
-                + artifactId + File.separator
-                + versionId;
-        return new File(artifactPath).listFiles();
+        Element urlElement = doc.createElement("url");
+        urlElement.setTextContent(repoUrl);
+        repositoryElement.appendChild(urlElement);
     }
 }
 
