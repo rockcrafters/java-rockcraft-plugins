b3707e9abe39b3d004a4fae41044e651e54ea194
Vladimir Petko <vladimir.petko@canonical.com>
2025-12-12 09:14:56 +1300

    chore: bump version to 1.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 allprojects {
-    version = "1.2.4"
+    version = "1.2.5"
     group = "io.github.rockcrafters"
     apply(plugin = "maven-publish")
     if (project.findProperty("gpr.repo") != null) {
