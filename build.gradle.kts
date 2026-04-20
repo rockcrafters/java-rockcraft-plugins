d1a2c424bb6439cb35944e130fb306a46f408a6b
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-20 13:18:03 +1200

    chore: update version to 1.2.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 allprojects {
-    version = "1.2.5"
+    version = "1.2.6"
     group = "io.github.rockcrafters"
     apply(plugin = "maven-publish")
     if (project.findProperty("gpr.repo") != null) {
